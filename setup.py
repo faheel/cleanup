c84416376e4b3ce3719c9089136c59177c64c80b
Faheel Ahmad <faheel@live.in>
2018-02-12 13:22:57 +0530

    Add setup files

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+    name='cleanup',
+    packages=['cleanup'],
+    version='0.0.0',
+    description='A simple command line utility that organises files in a directory into subdirectories.',
+    author='Faheel Ahmad',
+    author_email='faheel@live.in',
+    url='https://github.com/faheel/cleanup',
+    download_url='https://github.com/faheel/cleanup/archive/0.1.0.tar.gz',
+    keywords=[''],
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
