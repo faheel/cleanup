f263dcddb4c5c116a1200133e52c4bb6749a6f5b
Faheel Ahmad <faheel@live.in>
2018-04-04 13:05:33 +0530

    Implement option to revert cleanup of directories

--- cleanup/cleanup.py
+++ cleanup/cleanup.py
@@ -4,21 +4,27 @@
 Organise files in a directory into subdirectories.
 
 Usage:
-  cleanup [-d | -v] <dir>
+  cleanup [-d | -v] [-r] <dir>
   cleanup -h
 
 Options:
   -d, --dry-run     Do not make any changes, just display them.
   -v, --verbose     Display information while performing operations.
+  -r, --revert      Revert previous cleanup of the directory.
   -h, --help        Display this help text.
 '''
 
+import io
+import json
 import os
 
 from colored import stylize, fg, attr
 from docopt import docopt
 
 from file_types import FILE_TYPES
+
+REVERT_INFO_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                'revert_info.json')
 
 
 def get_longest_extension(filename):
@@ -41,14 +47,93 @@ def get_longest_extension(filename):
     return None
 
 
-def print_cleaning(action):
-    print(action + ' ' + stylize(root_dir, fg('light_blue') + attr('bold'))
-          + ' ...')
+def save_revert_info(revert_info):
+    with io.open(REVERT_INFO_FILE, 'w', encoding='utf8') as file:
+        file.write(json.dumps(revert_info, ensure_ascii=False, sort_keys=True,
+                              indent=4, separators=(',', ': ')))
 
 
-def print_move(move_action, file, file_type):
-    print(move_action + ' ' + stylize(file, attr('bold')) + ' under '
+def read_revert_info():
+    with io.open(REVERT_INFO_FILE) as file:
+        return json.load(file)
+
+
+def print_cleaning(action, dir):
+    print(action + ' ' + stylize(dir, fg('light_blue') + attr('bold'))
+          + ':')
+
+
+def print_move(move_action, file, file_type, revert=False, dry_run=False):
+    if revert:
+        preposition = 'from'
+    else:
+        preposition = 'under'
+    if dry_run:
+        move_action = stylize(move_action, fg('yellow'))
+    else:
+        move_action = stylize(move_action, fg('green'))
+    print(move_action + ' ' + stylize(file, attr('bold')) + ' ' + preposition + ' '
           + stylize(file_type, attr('bold')))
+
+
+def print_file_error(error, file, file_type, dry_run=False):
+    if dry_run:
+        preposition = 'from'
+    else:
+        preposition = 'under'
+    print(stylize(error, fg('red')) + ' ' + stylize(file, attr('bold')) + ' '
+          + preposition + ' ' + stylize(file_type, attr('bold')))
+
+
+def print_dir_error(error, dir):
+    print(stylize(error, fg('red')) + ': ' + stylize(dir, fg('light_blue')
+                                                          + attr('bold')))
+
+
+def print_complete(operation):
+    print(operation + ' ' + stylize('complete', fg('green')) + '!')
+
+
+def revert(dir_path):
+    abs_dir = os.path.abspath(dir_path)
+    try:
+        revert_info = read_revert_info()
+        if revert_info.get(abs_dir):
+            file_info_list = revert_info[abs_dir]
+        else:
+            # revert info about the specified directory is not available
+            raise Exception
+    except:
+        # revert info file doesn't exist, so cannot perform a revert
+        print('Nothing to do.')
+        exit()
+    
+    if dry_run:
+        print_cleaning('When reverting cleanup of', abs_dir)
+    elif verbose:
+        print_cleaning('Reverting cleanup of', abs_dir)
+    for file_info in file_info_list:
+        file_type = file_info['type']
+        file = file_info['name']
+        prev_path = os.path.join(abs_dir, file_type, file)
+        new_path = os.path.join(abs_dir, file)
+        try:
+            if dry_run:
+                if os.path.exists(prev_path):
+                    print_move('Will move back', file, file_type, revert=True, dry_run=True)
+                else:
+                    print_file_error('Will fail to move back', file, file_type, dry_run=True)
+            else:
+                os.renames(prev_path, new_path)
+                if verbose:
+                    print_move('Moved back', file, file_type, revert=True)
+        except:
+            print_file_error('Could not find', file, file_type)
+    if not dry_run:
+        if verbose:
+            print_complete('Revert')
+        revert_info.pop(abs_dir)
+        save_revert_info(revert_info)
 
 
 if __name__ == '__main__':
@@ -56,23 +141,48 @@ if __name__ == '__main__':
     dir_path = arguments['<dir>']   # path to the directory to be cleaned
     verbose = arguments['--verbose']
     dry_run = arguments['--dry-run']
-    
-    root_dir, dir_list, file_list = next(os.walk(dir_path), (None, [], []))
-    if dry_run:
-        print_cleaning('When cleaning up')
-    elif verbose:
-        print_cleaning('Cleaning up')
-    for file in file_list:
-        extension = get_longest_extension(file)
-        if extension:
-            original_name = os.path.join(root_dir, file)
-            file_type = FILE_TYPES[extension]
-            new_name = os.path.join(root_dir, file_type, file)
-            if dry_run:
-                print_move('Will move', file, file_type)
+    to_revert = arguments['--revert']
+
+    if to_revert:
+        revert(dir_path)
+    else:
+        root_dir, dir_list, file_list = next(os.walk(dir_path), (None, [], []))
+        if not root_dir:
+            print_dir_error('The specified directory does not exist',
+                            os.path.abspath(dir_path))
+            exit()
+        abs_path = os.path.abspath(root_dir)
+        if len(file_list) == 0:
+            print('Nothing to do.')
+            exit()
+        if dry_run:
+            print_cleaning('When cleaning up', abs_path)
+        elif verbose:
+            print_cleaning('Cleaning up', abs_path)
+        
+        revert_list = []
+        for file in file_list:
+            extension = get_longest_extension(file)
+            if extension:
+                original_name = os.path.join(root_dir, file)
+                file_type = FILE_TYPES[extension]
+                new_name = os.path.join(root_dir, file_type, file)
+                if dry_run:
+                    print_move('Will move', file, file_type, dry_run=True)
+                else:
+                    revert_list.append({
+                        'name': file,
+                        'type': file_type
+                    })
+                    os.renames(original_name, new_name)
+                    if verbose:
+                        print_move('Moved', file, file_type)
+        if not dry_run:
+            if verbose:
+                print_complete('Cleanup')
+            if os.path.exists(REVERT_INFO_FILE):
+                revert_info = read_revert_info()
             else:
-                os.renames(original_name, new_name)
-                if verbose:
-                    print_move('Moved', file, file_type)
-    if not dry_run and verbose:
-        print(stylize('Done', fg('green')) + '!')
+                revert_info = {}
+            revert_info[os.path.abspath(root_dir)] = revert_list
+            save_revert_info(revert_info)
