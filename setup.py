af9cb639f99c34c6b1af13185edc628cf1aa6622
Faheel Ahmad <faheel@live.in>
2018-04-06 23:29:30 +0530

    Increment revision version: v1.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='cleanup',
-    version='1.0.2',
+    version='1.0.3',
     description='A simple command line utility that organises files in a '
                 'directory into subdirectories.',
     long_description=long_description,
